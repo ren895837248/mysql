4b9b3cebd0b3dbff69a84baaeeface0e1a3774f1
ren895837248 <895837248@qq.com>
2017-03-23 09:54:56 +0800

    master slave

--- slave/docker-entrypoint.sh
+++ slave/docker-entrypoint.sh
@@ -167,6 +167,9 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 
 			echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
 		fi
+                echo "STOP SLAVE;" | "${mysql[@]}"
+                echo "CHANGE MASTER TO master_host='$MYSQL_MASTER_SERVICE_HOST', master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD' ;" | "${mysql[@]}"
+                echo "START SLAVE;" | "${mysql[@]}"                
 
 		echo
 		for f in /docker-entrypoint-initdb.d/*; do
